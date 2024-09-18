69697e7dc5f1acf35e95188664d7fa80c503b491
UtkarshBais13 <158749935+UtkarshBais13@users.noreply.github.com>
2024-09-18 23:14:08 +0530

    ghj

--- blog--ui/src/pages/home/Home.jsx
+++ blog--ui/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Header from "../../header/Header"
 import Posts from "../../posts/Posts"
 import Sidebar from "../../sidebar/Sidebar"
 import"./home.css"
-import axiosInstace from '../../config'
+import {axiosInstace} from '../../config'
 import { useLocation } from "react-router-dom"
 
 function Home() {
